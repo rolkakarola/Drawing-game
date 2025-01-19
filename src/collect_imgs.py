91866bcf28935c44133e7209f3f8738129797f48
Karolina Rola <145473759+rolkakarola@users.noreply.github.com>
2025-01-19 09:08:03 +0100

    Update collect_imgs.py

--- src/collect_imgs.py
+++ src/collect_imgs.py
@@ -1,3 +1,4 @@
+"""Capturing and saving images from a webcam"""
 import os
 import cv2
 
@@ -17,6 +18,7 @@ else:
     print("Camera is working!")
 
 for j in range(number_of_classes):
+    """For each class subdirectories are created to store images for that class"""
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
@@ -24,6 +26,7 @@ for j in range(number_of_classes):
 
     done = False
     while True:
+        """Opening a video capture, when q is pressed the data is being collected"""
         ret, frame = cap.read()
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
@@ -33,6 +36,7 @@ for j in range(number_of_classes):
 
     counter = 0
     while counter < dataset_size:
+        """Program captures 100 frames from the camera feed and saves them"""
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
